965170aedcfe4ea9c57c8e87f5db960921398f95
sigigi <bjh0321@gmail.com>
2019-07-09 13:30:17 +0900

    c# chunjiin upload
    c# chunjiin 버전 업로드

---
+++ dotnetChunjiinKeyPad/ChunjiinKeyPad/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ChunjiinKeyPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(572, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button1.Location = new System.Drawing.Point(165, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button2.Location = new System.Drawing.Point(27, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button3.Location = new System.Drawing.Point(165, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 68);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button4.Location = new System.Drawing.Point(303, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 68);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button5.Location = new System.Drawing.Point(27, 146);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 68);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "button1";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button6.Location = new System.Drawing.Point(165, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 68);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "button1";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button7.Location = new System.Drawing.Point(303, 146);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 68);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "button1";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button8.Location = new System.Drawing.Point(27, 220);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(132, 68);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "button1";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button9.Location = new System.Drawing.Point(165, 220);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(132, 68);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "button1";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button10.Location = new System.Drawing.Point(303, 220);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(132, 68);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "button1";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 18F);
+            this.button11.Location = new System.Drawing.Point(303, 294);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(132, 68);
+            this.button11.TabIndex = 1;
+            this.button11.Text = "␣";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(441, 72);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(132, 68);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "지우기";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(93, 294);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(66, 68);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "한/영";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(27, 294);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(66, 68);
+            this.button14.TabIndex = 2;
+            this.button14.Text = "숫자";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ dotnetChunjiinKeyPad/ChunjiinKeyPad/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChunjiinKeyPad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button[] btns = new Button[]
+            {
+                this.button1,
+                this.button2,
+                this.button3,
+                this.button4,
+                this.button5,
+                this.button6,
+                this.button7,
+                this.button8,
+                this.button9,
+                this.button10,
+                this.button11,
+                this.button12,
+                this.button13,
+            };
+            Chunjiin chunjiin = new Chunjiin(this.textBox1,btns);
+            
+        }
+    }
+}
